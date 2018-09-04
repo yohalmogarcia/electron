165ba25704541e3a912fd4e4b6c3f56c31de7458
yohalmogarcia <yohalmo.garcia@gmail.com>
2018-09-03 21:03:59 -0600

    intentos de escribir en un json

--- json-node/script.js
+++ json-node/script.js
@@ -30,6 +30,11 @@ function verTabla(){
 //sucede cuando el documento carga
 $(document).ready(function(){
 	
+	$("#form-modelos").submit(function(e){
+		e.preventDefault();
+	});
+
+
     //formulario
 	$('#guardar').click(function(e){
 				
@@ -39,8 +44,66 @@ $(document).ready(function(){
 	$('#btn').click(function(e){
 		verTabla();
 	});
+
+	$("#btn-modelo").click(function(e){
+		agregarAJSON();
+	});
     
-});
+});//fin de document.ready
+
+function agregarAJSON(){
+	var jsonData;
+	fs.readFileSync('modelos.json','utf-8',function(err,data){
+		alert("Hola");
+		if(err){
+			console.log(err);
+		}else{
+
+			var codigo_modelo=document.getElementById("codigo-modelo").value;
+			var nombre_modelo = document.getElementById("nombre-modelo").value;
+
+			jsonData=JSON.parse(data);
+			console.log("jsonData: "+jsonData);
+			var modelos = {
+				"codigo":codigo_modelo,
+				"nombreModelo":nombre_modelo
+			}
+			jsonData.push(modelos);
+			console.log("jsonData 2: "+jsonData);
+			var newdata = JSON.stringify(jsonData);
+			fs.writeFile('modelos.json', newdata,'utf-8', function(err){
+				if(err) throw err;
+				console.log("Exito!");
+			});
+		}
+	});//se adquieren los datos hexadecimales del archivo  
+
+
+/*
+
+
+	var jsonObj = JSON.parse(rawdata);  //se convierten los datos hexadecimales a json
+	var mod = jsonObj.data;
+	console.log("mod: "+mod);
+
+	var codigo_modelo=document.getElementById("codigo-modelo").value;
+	var nombre_modelo = document.getElementById("nombre-modelo").value;
+	var modelos = {
+		"codigo":codigo_modelo,
+		"nombreModelo":nombre_modelo
+	}
+
+	jsonObj.push(modelos);
+	var data = JSON.stringify(jsonObj);
+
+	console.log("data: "+data)
+
+	data+=mod;
+	console.log("data+modelos: "+data);
+	//fs.writeFileSync('modelos.json',data);
+
+	*/
+}
 
 
 
